41bc1dd269df99710d916a3c24039eab396a1173
AmandaJPicoli <amandajardimpicoli@gmail.com>
2021-02-03 14:07:29 -0300

    Exercicio aula 2, loja pagamento dinheiro

---
+++ Girls.Gama2/Entidades/Boleto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Girls.Gama2.Entidades
+{
+    public class Boleto
+    {
+        private const int DiasVencimento = 15;
+        private const double Juros = 0.10;
+
+        public Boleto(double valor, string cpf, string descricao)
+        {
+            Valor = valor;
+            Cpf = cpf;
+            DataEmissao = DateTime.Now;
+            Descricao = descricao;
+            Confirmacao = false;
+        }
+
+        public Guid CodigoBarra { get; set; }
+
+        public double Valor { get; set; }
+
+        public DateTime DataEmissao { get; set; }
+
+        public DateTime DataVencimento { get; set; }
+
+        public DateTime DataPagamento { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Confirmacao { get; set; }
+
+        public void GerarBoleto()
+        {
+            CodigoBarra = Guid.NewGuid();
+            DataVencimento = DataEmissao.AddDays(DiasVencimento);
+        }
+
+        public bool EstaPago()
+        {
+            return Confirmacao;
+        }
+
+        public bool EstaVencido()
+        {
+            return DataVencimento < DateTime.Now;
+        }
+
+        public void CalcularJuros()
+        {
+            var taxa = Valor * Juros;
+            Valor += taxa;
+        }
+
+        public void Pagar()
+        {
+            DataPagamento = DateTime.Now;
+            Confirmacao = true;
+        }
+    }
+}
